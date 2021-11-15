a0aa80dd0cbe41812c5ef66f2f7ca161bb0abc14
todemarie <93047347+todemarie@users.noreply.github.com>
2021-11-15 12:36:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var nom="tode";
+var statut="marie";
+let  doc="je suis" + nom + ""+ statut;
+console.log(doc);
+if(statut=="celibataire"){
+    console.log(nom+"vous avez droit à 50 000");    
+}
+else{
+    console.log(nom+"vous avez droit à 100 000F");
+}
+// var j=0
+//for (var i=0; i<= 10; i++){
+//    console.log(i);
+//}
+//var i=0;
+//do{
+  //  console.log(i);
+    //i++;
+//}while(i<=10);
+//var nombres= array(1,2,3,4,5);
+//var nombres=[,2,3,4,5];
+//console.log(nombres[0],nombres[3]);
+//for (var i of nombres){
+  //  console.log(i);
+//}
+function calcul(a,b){
+    return a+b;
+}
+//calcul();
+//console.log(calcul(4,5));
